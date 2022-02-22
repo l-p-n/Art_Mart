e88fd7a3119bba06c32b98260e60a7d759df8a90
Tim Halliday <thalliday793@gmail.com>
2022-02-22 12:35:16 +0000

    controller actions created

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -7,15 +7,25 @@ class ArtworksController < ApplicationController
   end
 
   def new
+    @artwork = Artwork.new
   end
 
   def create
+    @artwork = Artwork.new(artwork_params)
+    @artwork.save
+    redirect_to @artwork
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def artwork_params
+    params.require(artwork).permit(:title, :price_per_day, :artist, :style, :user_id, :description, :date)
   end
 
 end
