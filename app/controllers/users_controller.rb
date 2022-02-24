37cbe710a69ab076b593950f2f96a6fe2ba566b0
Susannah Taylor <susannahtay@gmail.com>
2022-02-24 14:51:57 +0000

    my account final

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,11 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @artworks = @user.artworks
-    @bookings = @user.artworks.bookings
+    # @bookings = @user.artworks.bookings
+    # @bookings.flatten
     @rentals = @user.bookings
+    @artworks_bookings = Booking.all.select do |booking|
+      booking.artwork.user == current_user
+    end
   end
 end
