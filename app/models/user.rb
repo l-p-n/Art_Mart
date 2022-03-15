1f20d3d1af2ca6ab3e92d248e1426204cc0a60ac
Susannah Taylor <susannahtay@gmail.com>
2022-03-15 09:50:44 +0000

    removed user validations blocking logins

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
 
   has_one_attached :photo
 
-  validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
