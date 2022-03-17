cd203857d35f78ca56cfb2c5a20a78eff268d62c
Luiza Passaglia Nascimento <luizapn@hotmail.com>
2022-03-17 11:43:30 +0000

    sign up fields added and devise parameters configured

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_one_attached :photo
 
   validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
